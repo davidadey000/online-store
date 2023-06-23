9d5282d735e05fa84a49df851a9961ece6874373
David Adediji <davidadey000@gmail.com>
2023-06-23 11:16:18 +0100

    Improve SearchResults

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchResults = () => {
   return (
-    <div className="h-[50px] w-full lg:w-[45%] rounded-b-md bg-white absolute top-full right-0">SearchResult</div>
+    <div className="h-[50px] w-full lg:w-[45%] rounded-b-md bg-white absolute top-full right-0 px-3">Search Result...</div>
   )
 }
 
